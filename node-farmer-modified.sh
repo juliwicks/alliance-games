0ccf158e1d924d8a8123ac8849d0796488f1d190
juliwicks <juliwicks@gmail.com>
2024-10-03 15:58:50 +0700

    Update node-farmer-modified.sh

--- node-farmer-modified.sh
+++ node-farmer-modified.sh
@@ -169,8 +169,8 @@ fi
 read -p "Enter the command you want to run on reboot (leave empty to skip): " autostart_command
 
 if [[ -n "$autostart_command" ]]; then
-    # Add command to crontab for autostart
-    (crontab -l 2>/dev/null; echo "@reboot $autostart_command") | crontab -
+    # Add command to crontab for autostart, navigate to /app first
+    (crontab -l 2>/dev/null; echo "@reboot cd /app && $autostart_command") | crontab -
     echo -e "${SUCCESS}The command has been added to crontab for autostart on reboot.${NC}"
 else
     echo -e "${INFO}No command provided for autostart on reboot. Skipping...${NC}"
@@ -183,16 +183,25 @@ echo -e "${INFO}Using generated MAC address: $mac_address${NC}"
 # Convert device_name to lowercase for the Docker image name
 device_name_lower=$(echo "$device_name" | tr '[:upper:]' '[:lower:]')
 
-# Step 7: Build the Docker image specific to this device
-echo -e "${INFO}Building the Docker image 'alliance_games_docker_$device_name_lower'...${NC}"
-docker build -t "alliance_games_docker_$device_name_lower" "$device_dir"
-
-echo -e "${SUCCESS}Congratulations! The Docker container '${device_name}' has been successfully set up with a fake UUID.${NC}"
-echo -e "${WARNING}Now copy and paste the 3rd command from AG Device Initialization board in the following command prompt...${NC}"
+# Step 7: Build the Docker image
+docker build -t "$device_name_lower" "$device_dir"
 
 # Step 8: Run the Docker container
+echo -e "${INFO}Running the Docker container...${NC}"
 if [[ "$use_proxy" == "Y" || "$use_proxy" == "y" ]]; then
-    docker run -it --cap-add=NET_ADMIN --mac-address="$mac_address" -v "$fake_product_uuid_file:/sys/class/dmi/id/product_uuid" --name="$device_name" "alliance_games_docker_$device_name_lower"
+    docker run -d --name "$device_name_lower" --privileged --net=host \
+    --env PRODUCT_UUID="$(cat $fake_product_uuid_file)" \
+    --env MAC_ADDRESS="$mac_address" \
+    --cap-add=NET_ADMIN \
+    --cap-add=NET_RAW \
+    -v "$device_dir:/app" "$device_name_lower"
 else
-    docker run -it --mac-address="$mac_address" -v "$fake_product_uuid_file:/sys/class/dmi/id/product_uuid" --name="$device_name" "alliance_games_docker_$device_name_lower"
+    docker run -d --name "$device_name_lower" --privileged --net=host \
+    --env PRODUCT_UUID="$(cat $fake_product_uuid_file)" \
+    --env MAC_ADDRESS="$mac_address" \
+    --cap-add=NET_ADMIN \
+    --cap-add=NET_RAW \
+    -v "$device_dir:/app" "$device_name_lower"
 fi
+
+echo -e "${SUCCESS}Docker container $device_name_lower has been started successfully!${NC}"
