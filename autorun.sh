04eb3a32831c97251fc3de5ed5104bbcb5fab153
juliwicks <juliwicks@gmail.com>
2024-10-04 01:04:07 +0700

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 
-# Navigate to the app directory
-cd /app || exit
+# Define the location of the autorun script and systemd service file
+autorun_script="/usr/local/bin/docker_autorun.sh"
+service_file="/etc/systemd/system/docker_autorun.service"
 
-# Prompt the user for the command to autorun
-echo "Please enter the command you want to autorun on reboot:"
-read -r command_input
+# Prompt for device name and custom run script
+read -p "Enter device name: " device_name
+read -p "Enter custom run script to execute: " custom_run_script
 
-# Create a script for the command
-echo "#!/bin/bash" > /app/autorun_command.sh
-echo "$command_input" >> /app/autorun_command.sh
+# Create the autorun script that will be executed on startup
+cat <<EOL > "$autorun_script"
+#!/bin/bash
 
-# Make the script executable
-chmod +x /app/autorun_command.sh
+# Start the Docker container and execute the custom command
+docker start "$device_name" && docker exec -it "$device_name" /bin/bash -c "sleep 2 && echo '$custom_run_script' && $custom_run_script"
+EOL
 
-# Create a service file to run the script on reboot
-cat <<EOF > /etc/systemd/system/autorun.service
+# Make the autorun script executable
+chmod +x "$autorun_script"
+
+# Create the systemd service file
+cat <<EOL > "$service_file"
 [Unit]
-Description=Autorun Command at Reboot
+Description=Docker Autorun Service
+After=docker.service
+Requires=docker.service
 
 [Service]
-ExecStart=/app/autorun_command.sh
-Restart=always
+Type=simple
+ExecStart=$autorun_script
+Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
-EOF
+EOL
 
-# Reload the systemd daemon to apply the new service
-systemctl daemon-reload
+# Reload the systemd daemon to recognize the new service
+sudo systemctl daemon-reload
 
-# Enable the service so it runs on startup
-systemctl enable autorun.service
+# Enable the service to start on boot
+sudo systemctl enable docker_autorun.service
 
-echo "The command has been set to autorun on reboot!"
+echo "Autorun script created at $autorun_script"
+echo "Systemd service created at $service_file"
+echo "The service has been enabled to run on startup."
